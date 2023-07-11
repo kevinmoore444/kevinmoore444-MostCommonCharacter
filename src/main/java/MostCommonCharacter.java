c7ec48752a74a1d273c65799671ca6e9e6e2b473
Kevin Moore <kevinmoore444@gmail.com>
2023-07-11 20:22:11 +0000

    Completed most commong character exercise

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +9,29 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        //Build Hashmap of letters and number of times that letter appeared.
+        for(int i=0; i<str.length(); i++){
+            if(map.get(str.charAt(i)) == null){
+                map.put(str.charAt(i), 1);
+            }
+            else{
+                int current_val = map.get(str.charAt(i));
+                map.put(str.charAt(i), current_val+1);
+            }
+        }
+
+        //Check map for greatest value and corresponding key
+        Character greatestKey = null;
+        int greatestValue = Integer.MIN_VALUE;
+
+        for(Character c : map.keySet()){
+            if(map.get(c) > greatestValue){
+                greatestValue = map.get(c);
+                greatestKey = c;
+            }
+        }
+        return greatestKey;
     }
 }
